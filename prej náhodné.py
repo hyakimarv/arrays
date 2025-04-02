bab2461292b982c3f0a89ddeda948504f9f38b38
hyakimarv <vitek.chvojka@infis.cz>
2025-04-02 14:26:34 +0200

    ovoce

---
+++ prej náhodné.py
@@ -0,0 +1,6 @@
+import random
+ovocnePole = ["Jablko", "Hruška", "Kiwii", "Banán", "Nektarinka"]
+ovoceNavic= input("Zadejte ovoce: ")
+ovocnePole.append(ovoceNavic)
+Ovocicko= random.choice(ovocnePole)
+print(Ovocicko);
